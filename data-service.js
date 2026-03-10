4bd299fb8ce0f5f95a9137fdc0ab664b468adf67
Bhupesh Chauhan <bhupeshch2000@gmail.com>
2026-03-10 14:22:11 -0400

    updated addEmployee and fixed employee route
    git push origin main
    
    okay

--- data-service.js
+++ data-service.js
@@ -36,6 +36,92 @@ module.exports.getAllEmployees = function () {
   });
 };
 
+module.exports.addEmployee = function (employeeData) {
+  return new Promise(function (resolve, reject) {
+    employeeData.isManager = employeeData.isManager ? true : false;
+    employeeData.employeeNum = employees.length + 1;
+    employees.push(employeeData);
+
+    resolve();
+  });
+};
+
+module.exports.getEmployeeByNum = function (num) {
+  return new Promise(function (resolve, reject) {
+    var foundEmployee = null;
+
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].employeeNum == num) {
+        foundEmployee = employees[i];
+      }
+    }
+
+    if (!foundEmployee) {
+      reject("query returned 0 results");
+      return;
+    }
+
+    resolve(foundEmployee);
+  });
+};
+
+module.exports.getEmployeesByStatus = function (status) {
+  return new Promise(function (resolve, reject) {
+    var filteredEmployeees = [];
+
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].status == status) {
+        filteredEmployeees.push(employees[i]);
+      }
+    }
+
+    if (filteredEmployeees.length == 0) {
+      reject("query returned 0 results");
+      return;
+    }
+
+    resolve(filteredEmployeees);
+  });
+};
+
+module.exports.getEmployeesByDepartment = function (department) {
+  return new Promise(function (resolve, reject) {
+    var filteredEmployeees = [];
+
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].department == department) {
+        filteredEmployeees.push(employees[i]);
+      }
+    }
+
+    if (filteredEmployeees.length == 0) {
+      reject("query returned 0 results");
+      return;
+    }
+
+    resolve(filteredEmployeees);
+  });
+};
+
+module.exports.getEmployeesByManager = function (manager) {
+  return new Promise(function (resolve, reject) {
+    var filteredEmployeees = [];
+
+    for (let i = 0; i < employees.length; i++) {
+      if (employees[i].employeeManagerNum == manager) {
+        filteredEmployeees.push(employees[i]);
+      }
+    }
+
+    if (filteredEmployeees.length == 0) {
+      reject("query returned 0 results");
+      return;
+    }
+
+    resolve(filteredEmployeees);
+  });
+};
+
 module.exports.getManagers = function () {
   return new Promise(function (resolve, reject) {
     var filteredEmployeees = [];
--- server.js
+++ server.js
@@ -2,14 +2,36 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const data = require("./data-service");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const multer = require("multer");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// multer requires a few options to be setup to store files with file extensions
+// by default it won't store extensions for security reasons
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function (req, file, cb) {
+    // we write the filename as the current date down to the millisecond
+    // in a large web service this would possibly cause a problem if two people
+    // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
+    // this is a simple example.
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+// tell multer to use the diskStorage function for naming files instead of the default.
+const upload = multer({ storage: storage });
+
 // Static assets
 app.use(express.static(path.join(__dirname, "public")));
+
+// body-parser configuration
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Home route
 app.get("/", (req, res) => {
@@ -26,18 +48,74 @@ app.get("/project", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "project.html"));
 });
 
-// employees route
+// add image route
+app.get("/images/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addImage.html"));
+});
+
+// add employee route
+app.get("/employees/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addEmployee.html"));
+});
+
+// to get the image names as array (image route)
+app.get("/images", (req, res) => {
+  fs.readdir("./public/images/uploaded", function (err, items) {
+    res.json({ images: items });
+  });
+});
+
+// employees routes (with queries)
 app.get("/employees", (req, res) => {
+  if (req.query.status) {
+    data
+      .getEmployeesByStatus(req.query.status)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: "no results" });
+      });
+  } else if (req.query.department) {
+    data
+      .getEmployeesByDepartment(req.query.department)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: "no results" });
+      });
+  } else if (req.query.manager) {
+    data
+      .getEmployeesByManager(req.query.manager)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: "no results" });
+      });
+  } else {
+    data
+      .getAllEmployees()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: "no results" });
+      });
+  }
+});
+
+// /employee/value route
+app.get("/employee/:empNum", (req, res) => {
   data
-    .getAllEmployees()
+    .getEmployeeByNum(req.params.empNum)
     .then((data) => {
       res.json(data);
     })
     .catch((err) => {
       res.json({ message: err });
     });
-
-  // res.send("Hello");
 });
 
 // managers route
@@ -48,7 +126,7 @@ app.get("/managers", (req, res) => {
       res.json(data);
     })
     .catch((err) => {
-      res.json({ message: err });
+      res.json({message: err});
     });
 });
 
@@ -60,19 +138,27 @@ app.get("/departments", (req, res) => {
       res.json(data);
     })
     .catch((err) => {
-      res.json({ message: err });
+      res.json({ message: "no results" });
     });
 });
 
-// ✅ Express v5-safe catch-all
-app.use((req, res) => {
-  res.redirect("/");
+// form data (employee data) upload POST method
+app.post("/employees/add", (req, res) => {
+  data.addEmployee(req.body).then(() => {
+    res.redirect("/employees");
+  });
+});
+
+// image upload POST method
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+  res.redirect("/images");
 });
 
 // 404 error handler for undefined routes
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
+
 
 // setup server
 data.initialize()
